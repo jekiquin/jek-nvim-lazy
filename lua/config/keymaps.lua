e4d4b1380ac2d90f1bbbdc529f968b47c692d138
jekiquin <jci.dev.0@gmail.com>
2025-06-14 22:34:20 -0700

    removed redundant quit key mapping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,5 +3,3 @@ vim.keymap.set({ "n", "v" }, "<C-j>", "5j")
 vim.keymap.set({ "n", "v" }, "<C-k>", "5k")
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from system cliboard" })
-vim.keymap.set("n", "<leader>qa", ":q<CR>", { desc = "Quit all" })
-
