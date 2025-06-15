fd3bfde7ae20d2f854195c8d7bcd1825a3f2c58a
jekiquin <jci.dev.0@gmail.com>
2025-06-14 22:35:30 -0700

    fixed conform styling

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,21 +1,67 @@
 return {
-  -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+	-- add gruvbox
+	{ "ellisonleao/gruvbox.nvim" },
 
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
-  },
+	-- Configure LazyVim to load gruvbox
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "gruvbox",
+		},
+	},
 
-  -- configure bufferline
-  {
-    "akinsho/bufferline.nvim",
-    keys = {
-      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next Buffer" },
-      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev Buffer" },
-    }
-  }
+	-- configure bufferline
+	{
+		"akinsho/bufferline.nvim",
+		keys = {
+			{ "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next Buffer" },
+			{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev Buffer" },
+		},
+	},
+
+	-- configure formatter
+	{
+		"stevearc/conform.nvim",
+		init = function()
+			-- disable autoformat as it sometimes take too long
+			vim.b.disable_autoformat = true
+			vim.g.disable_autoformat = true
+		end,
+		opts = function()
+			return {
+				formatters_by_ft = {
+					lua = { "stylua" },
+					javascript = { "prettier", "prettierd" },
+					javascriptreact = { "prettier", "prettierd" },
+					typescript = { "prettier", "prettierd" },
+					typescriptreact = { "prettier", "prettierd" },
+					vue = { "prettier", "prettierd" },
+					css = { "prettier", "prettierd" },
+					scss = { "prettier", "prettierd" },
+					less = { "prettier", "prettierd" },
+					html = { "prettier", "prettierd" },
+					json = { "prettier", "prettierd" },
+					jsonc = { "prettier", "prettierd" },
+					yaml = { "prettier", "prettierd" },
+					markdown = { "prettier", "prettierd" },
+					["markdown.mdx"] = { "prettier", "prettierd" },
+					graphql = { "prettier", "prettierd" },
+					handlebars = { "prettier", "prettierd" },
+				},
+			}
+		end,
+		keys = {
+			{
+				"<leader>cf",
+				function()
+					require("conform").format({
+						bufnr = vim.api.nvim_get_current_buf(),
+						timeout_ms = 10000,
+						lsp_format = "fallback",
+					})
+				end,
+				desc = "Format Buffer",
+			},
+		},
+	},
 }
