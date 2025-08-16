a5deb61db2d17dc87976a06e81126531ab4d2d6e
jekiquin <jci.dev.0@gmail.com>
2025-08-16 16:27:42 -0700

    added oil

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,32 @@
+local open_oil = function (dir)
+  local oil = require('oil')
+  oil.toggle_float(dir)
+end
+
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    watch_for_changes = true,
+    view_options = {
+      show_hidden = true
+    },
+    float = {
+      preview_split = "right"
+    },
+    keymaps = {
+      ["<C-j>"] = "actions.preview_scroll_down",
+      ["<C-k>"] = "actions.preview_scroll_up",
+    }
+  },
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+  keys = {
+    { "<Space>e", open_oil, desc = "Toggle Oil Float" },
+    { "<Space>E", function() open_oil(vim.fn.getcwd()) end, desc = "Toggle Oil Float (Project Root)" }
+  }
+}
