904b4b92ab0780ca07aab4a444c0308ee362ae8f
jekiquin <jci.dev.0@gmail.com>
2025-08-31 08:36:54 -0700

    added delmarks shortcut

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,10 @@ vim.keymap.set({ "n", "v" }, "[[j", "10j")
 vim.keymap.set({ "n", "v" }, "[[k", "10k")
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from system cliboard" })
+
+vim.keymap.set("n", "dm", function()
+  local char = vim.fn.getchar()
+  vim.cmd("delmarks " .. vim.fn.nr2char(char))
+end, { desc = "Delete mark" })
+
+vim.keymap.set("n", "<leader>dm", "<cmd>delmarks!<CR>", { desc = "Delete all marks in current buffer" })
